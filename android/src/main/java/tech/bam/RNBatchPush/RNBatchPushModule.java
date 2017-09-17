48c71517884452c63a25dec92f42dad1b6395e23
Louis Lagrange <lagrange.louis@gmail.com>
2017-09-17 17:14:01 +0200

    Android working version

--- android/src/main/java/tech/bam/RNBatchPush/RNBatchPushModule.java
+++ android/src/main/java/tech/bam/RNBatchPush/RNBatchPushModule.java
@@ -1,17 +1,35 @@
 package tech.bam.RNBatchPush;
 
+import android.app.Activity;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.batch.android.Batch;
+import com.batch.android.Config;
 
-public class RNBatchPushModule extends ReactContextBaseJavaModule {
+public class RNBatchPushModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
   private final ReactApplicationContext reactContext;
 
   public RNBatchPushModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    this.reactContext.addLifecycleEventListener(this);
+
+    Batch.Push.setGCMSenderId(BuildConfig.GCM_SERVER_API_KEY);
+    Batch.setConfig(new Config(BuildConfig.BATCH_API_KEY_ANDROID));
+
+    startBatch();
+  }
+
+  private void startBatch() {
+    Activity activity = getCurrentActivity();
+    if (activity == null)
+      return;
+
+    Batch.onStart(activity);
   }
 
   @ReactMethod
@@ -21,10 +39,35 @@ public class RNBatchPushModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void setCustomUserID(String userID) {
-    com.batch.android.Batch.User.editor()
+  public void loginUser(String userID) {
+    Batch.User.editor()
       .setIdentifier(userID)
       .save();
+  }
+
+  @ReactMethod
+  public void logoutUser() {
+    Batch.User.editor()
+      .setIdentifier(null)
+      .save();
+  }
+
+  @Override
+  public void onHostResume()
+  {
+    startBatch();
+  }
+
+  @Override
+  public void onHostPause()
+  {
+    Batch.onStop(getCurrentActivity());
+  }
+
+  @Override
+  public void onHostDestroy()
+  {
+    Batch.onDestroy(getCurrentActivity());
   }
 
   @Override
