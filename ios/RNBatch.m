9f22214566c8763b91fb7b6c6c0439562c03a852
Louis Lagrange <lagrange.louis@gmail.com>
2019-11-12 15:07:15 +0100

    Don't require main queue setup

--- ios/RNBatch.m
+++ ios/RNBatch.m
@@ -3,17 +3,12 @@
 
 @implementation RNBatch
 
-- (dispatch_queue_t)methodQueue
++ (BOOL)requiresMainQueueSetup
 {
-    return dispatch_get_main_queue();
+   return NO;
 }
-RCT_EXPORT_MODULE()
 
-- (id)init {
-    self = [super init];
-    
-    return self;
-}
+RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(start:(BOOL) doNotDisturb)
 {
@@ -74,12 +69,12 @@ RCT_EXPORT_METHOD(userData_save:(NSArray*)actions)
     BatchUserDataEditor *editor = [BatchUser editor];
     for (NSDictionary* action in actions) {
         NSString* type = action[@"type"];
-        
+
         // Set double, long, NSString, bool class values
         if([type isEqualToString:@"setAttribute"]) {
             [editor setAttribute:action[@"value"] forKey:action[@"key"]];
         }
-        
+
         // Handle dates
         // @TODO: prevent date parsing from erroring
         else if([type isEqualToString:@"setDateAttribute"]) {
@@ -88,39 +83,39 @@ RCT_EXPORT_METHOD(userData_save:(NSArray*)actions)
             NSDate *date = [NSDate dateWithTimeIntervalSince1970:unixTimeStamp];
             [editor setAttribute:date forKey:action[@"key"]];
         }
-        
+
         else if([type isEqualToString:@"removeAttribute"]) {
             [editor removeAttributeForKey:action[@"key"]];
         }
-        
+
         else if([type isEqualToString:@"clearAttributes"]) {
             [editor clearAttributes];
         }
-        
+
         else if([type isEqualToString:@"setIdentifier"]) {
             [editor setIdentifier:action[@"value"]];
         }
-        
+
         else if([type isEqualToString:@"setLanguage"]) {
             [editor setLanguage:action[@"value"]];
         }
-        
+
         else if([type isEqualToString:@"setRegion"]) {
             [editor setRegion:action[@"value"]];
         }
-        
+
         else if([type isEqualToString:@"addTag"]) {
             [editor addTag:action[@"tag"] inCollection:action[@"collection"]];
         }
-        
+
         else if([type isEqualToString:@"removeTag"]) {
             [editor removeTag:action[@"tag"] fromCollection:action[@"collection"]];
         }
-        
+
         else if([type isEqualToString:@"clearTagCollection"]) {
             [editor clearTagCollection:action[@"collection"]];
         }
-        
+
         else if([type isEqualToString:@"clearTags"]) {
             [editor clearTags];
         }
@@ -133,20 +128,20 @@ RCT_EXPORT_METHOD(userData_save:(NSArray*)actions)
 RCT_EXPORT_METHOD(userData_trackEvent:(NSString*)name label:(NSString*)label data:(NSDictionary*)serializedEventData)
 {
     BatchEventData *batchEventData = nil;
-    
+
     if ([serializedEventData isKindOfClass:[NSDictionary class]])
     {
         batchEventData = [BatchEventData new];
-        
+
         if (![serializedEventData isKindOfClass:[NSDictionary class]])
         {
             NSLog(@"RNBatch: Error while tracking event data: event data should be an object or null");
             return;
         }
-        
+
         NSArray<NSString*>* tags = serializedEventData[@"tags"];
         NSDictionary<NSString*, NSDictionary*>* attributes = serializedEventData[@"attributes"];
-        
+
         if (![tags isKindOfClass:[NSArray class]])
         {
             NSLog(@"RNBatch: Error while tracking event data: event data.tags should be an array");
@@ -157,7 +152,7 @@ RCT_EXPORT_METHOD(userData_trackEvent:(NSString*)name label:(NSString*)label dat
             NSLog(@"RNBatch: Error while tracking event data: event data.attributes should be a dictionnary");
             return;
         }
-        
+
         for (NSString *tag in tags)
         {
             if (![tag isKindOfClass:[NSString class]])
@@ -167,7 +162,7 @@ RCT_EXPORT_METHOD(userData_trackEvent:(NSString*)name label:(NSString*)label dat
             }
             [batchEventData addTag:tag];
         }
-        
+
         for (NSString *key in attributes.allKeys)
         {
             NSDictionary *typedAttribute = attributes[key];
@@ -176,10 +171,10 @@ RCT_EXPORT_METHOD(userData_trackEvent:(NSString*)name label:(NSString*)label dat
                 NSLog(@"RNBatch: Error while tracking event data: event data.attributes childrens should all be String/Dictionary tuples");
                 return;
             }
-            
+
             NSString *type = typedAttribute[@"type"];
             NSObject *value = typedAttribute[@"value"];
-            
+
             if ([@"string" isEqualToString:type]) {
                 if (![value isKindOfClass:[NSString class]])
                 {
@@ -214,7 +209,7 @@ RCT_EXPORT_METHOD(userData_trackEvent:(NSString*)name label:(NSString*)label dat
             }
         }
     }
-    
+
     [BatchUser trackEvent:name withLabel:label associatedData:batchEventData];
 }
 
@@ -225,7 +220,7 @@ RCT_EXPORT_METHOD(userData_trackTransaction:(double)amount data:(NSDictionary*)r
         NSLog(@"RNBatch: trackTransaction data should be an dictionary or nil");
         return;
     }
-    
+
     [BatchUser trackTransactionWithAmount:amount data:rawData];
 }
 
@@ -236,26 +231,26 @@ RCT_EXPORT_METHOD(userData_trackLocation:(NSDictionary*)serializedLocation)
         NSLog(@"RNBatch: Empty or null parameters for trackLocation");
         return;
     }
-    
+
     NSNumber *latitude = serializedLocation[@"latitude"];
     NSNumber *longitude = serializedLocation[@"longitude"];
     NSNumber *date = serializedLocation[@"date"]; // MS
     NSNumber *precision = serializedLocation[@"precision"];
-    
+
     if (![latitude isKindOfClass:[NSNumber class]])
     {
         NSLog(@"RNBatch: latitude should be a string");
         return;
     }
-    
+
     if (![longitude isKindOfClass:[NSNumber class]])
     {
         NSLog(@"RNBatch: longitude should be a string");
         return;
     }
-    
+
     NSTimeInterval ts = 0;
-    
+
     if (date)
     {
         if ([date isKindOfClass:[NSNumber class]]) {
@@ -265,9 +260,9 @@ RCT_EXPORT_METHOD(userData_trackLocation:(NSDictionary*)serializedLocation)
             return;
         }
     }
-    
+
     NSDate *parsedDate = ts != 0 ? [NSDate dateWithTimeIntervalSince1970:ts] : [NSDate date];
-    
+
     NSInteger parsedPrecision = 0;
     if (precision)
     {
@@ -278,7 +273,7 @@ RCT_EXPORT_METHOD(userData_trackLocation:(NSDictionary*)serializedLocation)
             return;
         }
     }
-    
+
     [BatchUser trackLocation:[[CLLocation alloc] initWithCoordinate:CLLocationCoordinate2DMake([latitude doubleValue], [longitude doubleValue])
                                                            altitude:0
                                                  horizontalAccuracy:parsedPrecision
@@ -294,12 +289,11 @@ RCT_EXPORT_METHOD(inbox_fetchNotifications:(RCTPromiseResolveBlock)resolve rejec
 {
     BatchInboxFetcher* fetcher = [BatchInbox fetcher];
     [fetcher fetchNewNotifications:^(NSError * _Nullable error, NSArray<BatchInboxNotificationContent *> * _Nullable notifications, BOOL foundNewNotifications, BOOL endReached) {
-        
+
         if (error) {
             reject(@"Inbox", @"Failed to fetch new notifications", error);
         }
-        
-        
+
     }];
 }
 
