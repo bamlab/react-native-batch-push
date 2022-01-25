6fb4790efae1fc107a03d66f2f979d15ad868fd3
theohdv <theodevilleneuve@gmail.com>
2022-01-25 13:36:03 +0100

    feat: keep compatibility wit old RN versions

--- ios/RNBatch.h
+++ ios/RNBatch.h
@@ -1,5 +1,14 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+#if __has_include("React/RCTBridgeModule.h")
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
+
+#if __has_include("React/RCTEventEmitter.h")
+  #import <React/RCTEventEmitter.h>
+#else
+  #import "RCTEventEmitter.h"
+#endif
 
 @import Batch;
 
